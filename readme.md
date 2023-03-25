dc23d53ceb9b49c7701aa86bca51075a2f0dc13a
shane0 <shane.null@gmail.com>
2023-03-25 11:08:02 -0700

    'week 1 workshop'

---
+++ readme.md
@@ -0,0 +1,9 @@
+# nucamp full stack course
+
+- <http://localhost:3000/>
+- start local server
+
+```sh
+npm i
+npm start 
+```
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# mkdir routes
+# touch routes/campsiteRouter.js 
+# touch w1/node-express/routes/promotionRouter.js 
+# touch w1/node-express/routes/partnerRouter.js 
